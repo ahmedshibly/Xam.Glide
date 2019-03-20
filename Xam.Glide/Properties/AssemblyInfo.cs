14fb02bac84c3e617e9180deb6ec38eccd641701
ahmednayem <shibly77@outlook.com>
2019-03-20 08:16:04 +0600

    4.7

--- Xam.Glide/Properties/AssemblyInfo.cs
+++ Xam.Glide/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Xam.Glide.GifDecoder")]
+[assembly: AssemblyTitle("Xam.Glide")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Xam.Glide.GifDecoder")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("Xam.Glide")]
+[assembly: AssemblyCopyright("Nayem Ahmed Shibly")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
